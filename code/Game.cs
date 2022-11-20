15a812729672914f149eb6279286b50351998345
John Schneider <a1steaksa@users.noreply.github.com>
2022-11-20 12:35:53 -0600

    Rebased onto Sandbox

--- code/Game.cs
+++ code/Game.cs
@@ -1,44 +1,211 @@
 ﻿using Sandbox;
-using Sandbox.UI.Construct;
-using System;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Sandbox.PlayerController;
+using static Sandbox.Package;
 
-namespace Sandbox;
-
-/// <summary>
-/// This is your game class. This is an entity that is created serverside when
-/// the game starts, and is replicated to the client. 
-/// 
-/// You can use this to create things like HUDs and declare which player class
-/// to use for spawned players.
-/// </summary>
-public partial class MyGame : Sandbox.Game {
-	public MyGame() {}
-
-	/// <summary>
-	/// A client has joined the server. Make them a pawn to play with
-	/// </summary>
-	public override void ClientJoined( Client client ) {
-		base.ClientJoined( client );
-
-		// Create a pawn for this client to play with
-		Player player = new Player();
-		client.Pawn = player;
-
-		// Get all of the spawnpoints
-		var spawnpoints = Entity.All.OfType<SpawnPoint>();
-
-		// chose a random one
-		var randomSpawnPoint = spawnpoints.OrderBy( x => Guid.NewGuid() ).FirstOrDefault();
-
-		// if it exists, place the pawn there
-		if ( randomSpawnPoint != null ) {
-			var tx = randomSpawnPoint.Transform;
-			tx.Position = tx.Position + Vector3.Up * 50.0f; // raise it up
-			player.Transform = tx;
+partial class SandboxGame : Game {
+	public SandboxGame() {
+		if ( IsServer ) {
+			// Create the HUD
+			_ = new SandboxHud();
 		}
 	}
+
+	public override void ClientJoined( Client cl ) {
+		base.ClientJoined( cl );
+		var player = new SandboxPlayer( cl );
+		player.Respawn();
+
+		cl.Pawn = player;
+	}
+
+	protected override void OnDestroy() {
+		base.OnDestroy();
+	}
+
+	[ConCmd.Server( "spawn" )]
+	public static async Task Spawn( string modelname ) {
+		var owner = ConsoleSystem.Caller?.Pawn;
+
+		if ( ConsoleSystem.Caller == null ) {
+			return;
+		}
+
+		var tr = Trace.Ray( owner.EyePosition, owner.EyePosition + owner.EyeRotation.Forward * 500 )
+			.UseHitboxes()
+			.Ignore( owner )
+			.Run();
+
+		var modelRotation = Rotation.From( new Angles( 0, owner.EyeRotation.Angles().yaw, 0 ) ) * Rotation.FromAxis( Vector3.Up, 180 );
+
+		//
+		// Does this look like a package?
+		//
+		if ( modelname.Count( x => x == '.' ) == 1 && !modelname.EndsWith( ".vmdl", System.StringComparison.OrdinalIgnoreCase ) && !modelname.EndsWith( ".vmdl_c", System.StringComparison.OrdinalIgnoreCase ) ) {
+			modelname = await SpawnPackageModel( modelname, tr.EndPosition, modelRotation, owner );
+			if ( modelname == null )
+				return;
+		}
+
+		var model = Model.Load( modelname );
+		if ( model == null || model.IsError )
+			return;
+
+		var ent = new Prop {
+			Position = tr.EndPosition + Vector3.Down * model.PhysicsBounds.Mins.z,
+			Rotation = modelRotation,
+			Model = model
+		};
+
+		// Let's make sure physics are ready to go instead of waiting
+		ent.SetupPhysicsFromModel( PhysicsMotionType.Dynamic );
+
+		// If there's no physics model, create a simple OBB
+		if ( !ent.PhysicsBody.IsValid() ) {
+			ent.SetupPhysicsFromOBB( PhysicsMotionType.Dynamic, ent.CollisionBounds.Mins, ent.CollisionBounds.Maxs );
+		}
+	}
+
+	static async Task<string> SpawnPackageModel( string packageName, Vector3 pos, Rotation rotation, Entity source ) {
+		var package = await Package.Fetch( packageName, false );
+		if ( package == null || package.PackageType != Package.Type.Model || package.Revision == null ) {
+			// spawn error particles
+			return null;
+		}
+
+		if ( !source.IsValid ) return null; // source entity died or disconnected or something
+
+		var model = package.GetMeta( "PrimaryAsset", "models/dev/error.vmdl" );
+		var mins = package.GetMeta( "RenderMins", Vector3.Zero );
+		var maxs = package.GetMeta( "RenderMaxs", Vector3.Zero );
+
+		// downloads if not downloads, mounts if not mounted
+		await package.MountAsync();
+
+		return model;
+	}
+
+	[ConCmd.Server( "spawn_entity" )]
+	public static void SpawnEntity( string entName ) {
+
+		//Test
+		var owner = ConsoleSystem.Caller.Pawn as Player;
+
+		if ( owner == null )
+			return;
+
+		var entityType = TypeLibrary.GetDescription<Entity>( entName )?.TargetType;
+		if ( entityType == null )
+			return;
+
+		if ( !TypeLibrary.Has<SpawnableAttribute>( entityType ) )
+			return;
+
+		var tr = Trace.Ray( owner.EyePosition, owner.EyePosition + owner.EyeRotation.Forward * 200 )
+			.UseHitboxes()
+			.Ignore( owner )
+			.Size( 2 )
+			.Run();
+
+		var ent = TypeLibrary.Create<Entity>( entityType );
+		if ( ent is BaseCarriable && owner.Inventory != null ) {
+			if ( owner.Inventory.Add( ent, true ) )
+				return;
+		}
+
+		ent.Position = tr.EndPosition;
+		ent.Rotation = Rotation.From( new Angles( 0, owner.EyeRotation.Angles().yaw, 0 ) );
+
+		//Log.Info( $"ent: {ent}" );
+	}
+
+	public override void DoPlayerNoclip( Client player ) {
+		if ( player.Pawn is Player basePlayer ) {
+			if ( basePlayer.DevController is NoclipController ) {
+				Log.Info( "Noclip Mode Off" );
+				basePlayer.DevController = null;
+			} else {
+				Log.Info( "Noclip Mode On" );
+				basePlayer.DevController = new NoclipController();
+			}
+		}
+	}
+
+	[ClientRpc]
+	internal static void RespawnEntitiesClient() {
+		Map.Reset( DefaultCleanupFilter );
+	}
+
+	[ConCmd.Admin( "respawn_entities" )]
+	public static void RespawnEntities() {
+		Map.Reset( DefaultCleanupFilter );
+		RespawnEntitiesClient();
+	}
+
+	[ClientRpc]
+	public override void OnKilledMessage( long leftid, string left, long rightid, string right, string method ) {
+		KillFeed.Current?.AddEntry( leftid, left, rightid, right, method );
+	}
+
+	[ConCmd.Server( "spawnpackage" )]
+	public static async Task SpawnPackage( string fullIdent ) {
+		var owner = ConsoleSystem.Caller.Pawn as Player;
+
+		if ( owner == null )
+			return;
+
+		Log.Info( $"Spawn package {fullIdent}" );
+
+		var package = await Package.FetchAsync( fullIdent, false );
+		if ( package == null ) {
+			Log.Warning( $"Tried to spawn package {fullIdent} - which was not found" );
+			return;
+		}
+
+		Log.Info( $"Spawn package {package.Title}" );
+
+		var entityname = package.GetMeta( "PrimaryAsset", "" );
+
+		if ( string.IsNullOrEmpty( entityname ) ) {
+			Log.Warning( $"{package.FullIdent} doesn't have a PrimaryAsset key" );
+			return;
+		}
+
+		if ( !CanSpawnPackage( package ) ) {
+			Log.Warning( $"Not allowed to spawn package {package.FullIdent}" );
+			return;
+		}
+
+		await package.MountAsync( true );
+
+		Log.Info( $"Spawning Entity: {entityname}" );
+
+		var type = TypeLibrary.GetDescription( entityname );
+		if ( type == null ) {
+			Log.Warning( $"'{entityname}' type wasn't found for {package.FullIdent}" );
+			return;
+		}
+
+		Log.Info( $"Found Type: {type.Name}" );
+		Log.Info( $"		  : {type.ClassName}" );
+
+		var ent = type.Create<Entity>();
+
+		var tr = Trace.Ray( owner.EyePosition, owner.EyePosition + owner.EyeRotation.Forward * 200 )
+							.UseHitboxes()
+							.Ignore( owner )
+							.Size( 2 )
+							.Run();
+
+		ent.Position = tr.EndPosition;
+		ent.Rotation = Rotation.From( new Angles( 0, owner.EyeRotation.Angles().yaw, 0 ) );
+	}
+
+	static bool CanSpawnPackage( Package package ) {
+		if ( package.PackageType != Package.Type.Addon ) return false;
+		if ( !package.Tags.Contains( "runtime" ) ) return false;
+
+		return true;
+	}
+
 }
